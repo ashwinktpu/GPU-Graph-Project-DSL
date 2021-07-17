77a9e48c43262519c44c0ce4cc83da7ea3a26ffa
Ashwina Kumar <kumarashwina31@gmail.com>
2021-07-17 20:57:43 +0530

    Update SSSP.cu

--- SSSP.cu
+++ SSSP.cu
@@ -6,23 +6,24 @@
 
 __global__ void SSSP_Kernel(int * gpu_OA , int * gpu_edgeList , int* weight, int * gpu_dist , int src ,int V, int MAX_VAL) {
 
-
 unsigned int id = threadIdx.x + (blockDim.x * blockIdx.x);
-
  
+ if(id ==0  || id ==1){
  for (int edge = gpu_OA[id]; edge < gpu_OA[id+1]; edge ++) 
  {
-          //printf("inside inner for loop\n");
+         //printf("id= %d tid=%d ",gpu_OA[id],gpu_OA[id+1]);
           int nbr = gpu_edgeList[edge] ;
+          printf("nbrr=%d ",nbr);
           int e = edge;
           int dist_new = gpu_dist[id] + weight[e];
           
            if (gpu_dist[id] == MAX_VAL){
                 dist_new = MAX_VAL;
             }
+            
           if (gpu_dist[nbr] > dist_new)
           {
-            bool modified_new = true;
+            //bool modified_new = true;
            // omp_set_lock(&(lock[nbr])) ;
             if (gpu_dist[nbr] > dist_new)
             {
@@ -34,22 +35,14 @@ unsigned int id = threadIdx.x + (blockDim.x * blockIdx.x);
             //omp_unset_lock(&(lock[nbr]));
           }
    }
+   }
 
 
 } 
 
-void SSSP(int * OA , int * edgeList , int* cpu_edgeLen , int * dist , int src ,int V)
+void SSSP(int * OA , int * edgeList , int* cpu_edgeLen , int * dist , int src ,int V, int E)
 {
-
-  //printf("inside SSSP cpu call\n");
-  
-  printf("%d",V);
-  
   int MAX_VAL = 2147483647 ;
-
-   for(int i=0; i<= V; i++) {
-    printf("%d",OA[i]);
-  }
   
   int *gpu_edgeList;
   int *gpu_edgeLen;
@@ -57,8 +50,8 @@ void SSSP(int * OA , int * edgeList , int* cpu_edgeLen , int * dist , int src ,i
   int *gpu_OA;
 
   cudaMalloc( &gpu_OA, sizeof(int) * (1+V) );
-  cudaMalloc( &gpu_edgeList, sizeof(int) * (1+V) );
-  cudaMalloc( &gpu_edgeLen, sizeof(int) * (V) );
+  cudaMalloc( &gpu_edgeList, sizeof(int) * (E) );
+  cudaMalloc( &gpu_edgeLen, sizeof(int) * (E) );
   cudaMalloc( &gpu_dist, sizeof(int) * (V) );
   
   unsigned int block_size;
@@ -94,21 +87,18 @@ void SSSP(int * OA , int * edgeList , int* cpu_edgeLen , int * dist , int src ,i
   bool finished = false;
   
   cudaMemcpy(gpu_OA, OA, sizeof(int) * (1+V), cudaMemcpyHostToDevice);
-  cudaMemcpy(gpu_edgeList, edgeList, sizeof(int) * (1+V), cudaMemcpyHostToDevice);
-  cudaMemcpy(gpu_edgeLen,cpu_edgeLen , sizeof(int) * (V), cudaMemcpyHostToDevice);
+  cudaMemcpy(gpu_edgeList, edgeList, sizeof(int) * (E), cudaMemcpyHostToDevice);
+  cudaMemcpy(gpu_edgeLen,cpu_edgeLen , sizeof(int) * (E), cudaMemcpyHostToDevice);
   cudaMemcpy(gpu_dist,dist , sizeof(int) * (V), cudaMemcpyHostToDevice);
   
   
-  //while ( !finished )
-  //{
-    
+ // while ( !finished )
+ // {
       //if (modified[id] == true ){
-       // modified[index] = false;
-       
+      //  modified[index] = false;
       SSSP_Kernel<<<num_blocks , block_size>>>(gpu_OA,gpu_edgeList, gpu_edgeLen ,gpu_dist,src, V ,MAX_VAL);
    // }
     //bool modified_fp = false ;
-    //#pragma omp parallel for reduction(||:modified_fp)
    // for (int v = 0; v < V; v ++) 
     //modified_fp = modified_fp || modified[v] ;
     //finished = !modified_fp ;
@@ -142,23 +132,23 @@ void SSSP(int * OA , int * edgeList , int* cpu_edgeLen , int * dist , int src ,i
 // driver program to test above function
 int main(int argc , char ** argv)
 {
-  graph G("/home/ashwina/cuda/input2.txt");
+  graph G("/home/ashwina/cuda/final/input4.txt");
   G.parseGraph();
    
-   int V = G.num_nodes();
+  int V = G.num_nodes();
 //---------------------------------------//   
-   printf("number of nodes =%d\n",V);
+   //printf("number of nodes =%d\n",V);
+//-------------------------------------// 
+ int E = G.num_edges();
+ 
+ //---------------------------------------//   
+  // printf("number of edges =%d\n",E);
 //-------------------------------------// 
   
   int* edgeLen = G.getEdgeLen();
   
-  for(int i=0;i<V;i++) {
-    printf("edgeLen = %d\n",edgeLen[i]);
-  }
-  
   int* dist;
-  //int* dist_next=new int[G.num_nodes()+1];
-  //int* parent=new int[G.num_nodes()+1];
+
   int src=0;
   
   int *OA;
@@ -166,56 +156,27 @@ int main(int argc , char ** argv)
   int *cpu_edgeLen;
   
   
-  
    OA = (int *)malloc( (V+1)*sizeof(int));
-   edgeList = (int *)malloc( (V+1)*sizeof(int));
-   cpu_edgeLen = (int *)malloc( (V)*sizeof(int));
-   dist = (int *)malloc( (V+1)*sizeof(int));
-   
-   
-  
-  for(int i=0; i<= V; i++) {
-    printf("%d",G.indexofNodes[i]);
-  }
+   edgeList = (int *)malloc( (E)*sizeof(int));
+   cpu_edgeLen = (int *)malloc( (E)*sizeof(int));
+   dist = (int *)malloc( (V)*sizeof(int));
   
   for(int i=0; i<= V; i++) {
     int temp = G.indexofNodes[i];
     OA[i] = temp;
   }
   
-    printf("\n");
-  
-  for(int i=0; i<= V; i++) {
-    printf("%d",OA[i]);
-  }
-  
-      printf("\n");
-  
-  for(int i=0;i<=V;i++) {
-    printf("%d",  G.edgeList[i]);
-  }
-  
-   printf("\n");
-  
-  for(int i=0; i<= V; i++) {
+  for(int i=0; i< E; i++) {
     int temp = G.edgeList[i];
     edgeList[i] = temp;
   }
-    printf("\n");
-   for(int i=0; i<= V; i++) {
-    printf("%d",edgeList[i]);
-  }
   
-  for(int i=0; i< V; i++) {
+  for(int i=0; i< E; i++) {
     int temp = edgeLen[i];
     cpu_edgeLen[i] = temp;
   }
-  
-  for(int i=0;i<V;i++){
-    printf("%d",cpu_edgeLen[i]);
-  }
 
-   SSSP(OA,edgeList, cpu_edgeLen ,dist,src, V);
+  SSSP(OA,edgeList, cpu_edgeLen ,dist,src, V,E);
  
 
   return 0;
